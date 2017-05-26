86d38161303d1853b4845507fc38923d2a679d72
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2017-05-26 10:08:35 -0400

    teste maroto;

---
+++ node/ServerT.js
@@ -0,0 +1,16 @@
+/**
+ * Created by marcos on 26/05/17.
+ */
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var app = express();
+
+var options = {
+    cert: fs.readFileSync('/etc/letsencrypt/csr/0000_csr-certbot.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/keys/0000_key-certbot.pem')
+};
+
+http.createServer(app).listen(8080);
+https.createServer(options, app).listen(443);
