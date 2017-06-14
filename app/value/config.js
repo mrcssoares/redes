cfd9e8bbe089911e8aa906524acefbe735b4566a
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2017-06-13 23:03:29 -0400

    teste

--- app/value/config.js
+++ app/value/config.js
@@ -7,5 +7,5 @@ angular.module("song").value("config", {
     //baseUrl: "https://api.upawa.com.br:8080",// ta na amazon
     apikey: "redesEtop",
     songKey: "AAAActD0Jgg:APA91bFNhrZ9j_uPWtGnRfUwY-_eOh9oWqpbq7CymJtgtgB-SG9osBKT8-tmpXLsSJAsfXNSjrLYVSGvErNBWonDsbl5T4BQvzI4FjFVKx3Q4VCVjUI-CYN1TRYp5XPpIYvG8V2ayGPX"
-
+    //comentario maroto
 });
