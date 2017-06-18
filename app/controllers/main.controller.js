121a4c3181457549554490d35336342d9bd9af42
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2017-06-18 00:18:19 -0400

    teste3

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -103,6 +103,23 @@ angular.module("song").controller('mainController', function ($scope, objectUser
             });
     }
 
+    var havePermission = window.webkitNotifications.checkPermission();
+    if (havePermission == 0) {
+        // 0 is PERMISSION_ALLOWED
+        var notification = window.webkitNotifications.createNotification(
+            'http://i.stack.imgur.com/dmHl0.png',
+            'Chrome notification!',
+            'Here is the notification text'
+        );
+
+        notification.onclick = function () {
+            window.open("https://stackoverflow.com/a/13328397/1269037");
+            notification.close();
+        }
+        notification.show();
+    } else {
+        window.webkitNotifications.requestPermission();
+    }
     //escuta novas notificações
     messaging.onMessage(function(payload) {
         console.log("Message received. ", payload);
